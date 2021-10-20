3554e709f8ef2822b1ea56be6a35b4f237b68a73
Ibra <ibraennara@gmail.com>
2021-10-20 16:33:57 -0400

    fix: Validating input

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,16 @@
 import { updateOutput } from "./communication.js";
 
 // EVENT LISTENERS
-document.getElementById("minusLevel").addEventListener("click", minusLevel)
-document.getElementById("plusLevel").addEventListener("click", plusLevel)
-document.getElementById("minusSTR").addEventListener("click", minusSTR)
-document.getElementById("plusSTR").addEventListener("click", plusSTR)
-document.getElementById("minusSTA").addEventListener("click", minusSTA)
-document.getElementById("plusSTA").addEventListener("click", plusSTA)
-document.getElementById("minusDEX").addEventListener("click", minusDEX)
-document.getElementById("plusDEX").addEventListener("click", plusDEX)
-document.getElementById("minusINT").addEventListener("click", minusINT)
-document.getElementById("plusINT").addEventListener("click", plusINT)
+document.getElementById("minusLevel").addEventListener("click", minusLevel);
+document.getElementById("plusLevel").addEventListener("click", plusLevel);
+document.getElementById("minusSTR").addEventListener("click", minusSTR);
+document.getElementById("plusSTR").addEventListener("click", plusSTR);
+document.getElementById("minusSTA").addEventListener("click", minusSTA);
+document.getElementById("plusSTA").addEventListener("click", plusSTA);
+document.getElementById("minusDEX").addEventListener("click", minusDEX);
+document.getElementById("plusDEX").addEventListener("click", plusDEX);
+document.getElementById("minusINT").addEventListener("click", minusINT);
+document.getElementById("plusINT").addEventListener("click", plusINT);
 
 const job = document.getElementById('job');
 const level = document.getElementById('level');
@@ -31,8 +31,8 @@ DEX.addEventListener("change", updateStats);
 INT.addEventListener("change", updateStats);
 assistBuffs.addEventListener("change", updateStats);
 
-const tooltips_arr = Array.prototype.slice.call(tooltips);
-tooltips_arr.forEach(tooltip => {
+const tooltipsArr = Array.prototype.slice.call(tooltips);
+tooltipsArr.forEach(tooltip => {
     tooltip.addEventListener("mouseover", (event) => {
         let text = tooltip.parentNode.getElementsByTagName("p")[0];
         activeTooltip.getElementsByTagName('h5')[0].innerText = text.innerText;
@@ -130,5 +130,27 @@ function minusINT() {
 }
 
 function updateStats() {
+    validateInput();
     updateOutput(job.value, STR.value, STA.value, DEX.value, INT.value, level.value, assistint.value);
+}
+
+function validateInput() {
+    const levelValue = parseInt(level.value);
+    const strValue = parseInt(STR.value);
+    const staValue = parseInt(STA.value);
+    const dexValue = parseInt(DEX.value);
+    const intValue = parseInt(INT.value);
+
+    level.value = levelValue < 1 ? 1 : level.value;
+    level.value = levelValue > 120 ? 120 : level.value;
+    STR.value = strValue < 15 ? 15 : STR.value;
+    STA.value = staValue < 15 ? 15 : STA.value;
+    DEX.value = dexValue < 15 ? 15 : DEX.value;
+    INT.value = intValue < 15 ? 15 : INT.value;
+
+    level.value = isNaN(level.value) ? 15 : level.value;
+    STR.value = isNaN(STR.value) ? 15 : STR.value;
+    STA.value = isNaN(STA.value) ? 15 : STA.value;
+    DEX.value = isNaN(DEX.value) ? 15 : DEX.value;
+    INT.value = isNaN(INT.value) ? 15 : INT.value;   
 }
