3ac7a22cb9f46da070182535e510f68b7f93c7f8
Ibra <ibraennara@gmail.com>
2022-02-23 17:15:51 -0500

    Changelog

--- src/components/Leftbar/Changelog.vue
+++ src/components/Leftbar/Changelog.vue
@@ -9,6 +9,7 @@
         <li>Added an Apply button to the character control window.</li>
         <li>Added a reset button to the character control window.</li>
         <li>The Skill Damage stat is now properly reflected.</li>
+        <li>Base stats from all gear is now added properly.</li>
       </ul>
     </div>
     <h5 style="opacity: 0.5;">This section will only outline notable changes. If you would like to follow development more closely, visit the <a href="https://github.com/Frostiae/Flyffulator">GitHub</a> page.</h5>
